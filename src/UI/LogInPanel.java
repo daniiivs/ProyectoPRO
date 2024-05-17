5246569d4c5d93f007e5d79f04dcf10edb395086
Dani <danielvs90@gmail.com>
2024-05-17 23:51:25 +0100

    Login
    Implemented a Log In option

--- src/UI/LogInPanel.java
+++ src/UI/LogInPanel.java
@@ -1,23 +1,82 @@
 package UI;
 
-import javax.swing.JPanel;
-import javax.swing.JButton;
-import java.awt.event.ActionListener;
+import Entities.Doctors;
+
+import javax.swing.*;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class LogInPanel extends JPanel {
-	JButton btnNewButton = new JButton("Back");
+	private JLabel dniLabel;
+	private JLabel passwordLabel;
+
+	private JTextField dniField;
+	private JPasswordField passwordField;
+
+	private JButton logInButton;
+	protected JButton backButton;
 
 	public LogInPanel() {
 		setLayout(null);
-		
-		btnNewButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		btnNewButton.setBounds(71, 51, 89, 23);
-		add(btnNewButton);
-
+		editLabels();
+		editInputs();
+		editButtons();
 	}
 
+	private void editButtons() {
+		logInButton = new JButton("Log In");
+		logInButton.setBounds(119, 200, 89, 23);
+		logInButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if (!UtilityMethods.dniIsRegistered(dniField.getText())) {
+					dniField.setText("");
+					JOptionPane.showMessageDialog(null, "The DNI you typed is not registered", "Error", JOptionPane.ERROR_MESSAGE);
+				} else if (!UtilityMethods.passwordNotMatching(String.valueOf(passwordField.getPassword()), dniField.getText())) {
+					passwordField.setText("");
+					JOptionPane.showMessageDialog(null, "Incorrect password. Please try again", "Error", JOptionPane.ERROR_MESSAGE);
+				} else {
+					UtilityMethods.logIn(dniField.getText());
+					resetInputs();
+				}
+			}
+		});
+		add(logInButton);
+
+		backButton = new JButton("Back");
+		backButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				resetInputs();
+			}
+		});
+		backButton.setBounds(375, 200, 89, 23);
+		add(backButton);
+	}
+
+	private void resetInputs() {
+		dniField.setText("");
+		passwordField.setText("");
+	}
+
+	private void editInputs() {
+		dniField = new JTextField();
+		dniField.setBounds(316, 50, 200, 20);
+		add(dniField);
+		dniField.setColumns(10);
+
+		passwordField = new JPasswordField();
+		passwordField.setBounds(316, 100, 200, 20);
+		add(passwordField);
+	}
+
+	private void editLabels() {
+		dniLabel = new JLabel("DNI:");
+		dniLabel.setBounds(145, 50, 161, 20);
+		add(dniLabel);
+
+		passwordLabel = new JLabel("Password:");
+		passwordLabel.setBounds(145, 100, 161, 20);
+		add(passwordLabel);
+	}
 }
--- src/UI/WelcomeFrame.java
+++ src/UI/WelcomeFrame.java
@@ -25,20 +25,18 @@ public class WelcomeFrame extends JFrame {
 		logInPanel = new LogInPanel();
 		signUpPanel = new SignUpPanel();
 
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(new Rectangle(1000, 600));
-		cardPanel = new JPanel();
-		cardPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
-
-		setContentPane(cardPanel);
-		cardPanel.setLayout(new CardLayout(0, 0));
+		editFrame();
+		editPanel();
 		
 		//Add Panels on window
 		cardPanel.add(welcomePanel, WELCOMEPANEL);
 		cardPanel.add(logInPanel, LOGINPANEL);
 		cardPanel.add(signUpPanel, SIGNUPPANEL);
 
+		editCardButtons();
+	}
 
+	private void editCardButtons() {
 		welcomePanel.btnLogIn.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
@@ -46,7 +44,7 @@ public class WelcomeFrame extends JFrame {
 				layout.show(cardPanel, LOGINPANEL);
 			}
 		});
-		
+
 		welcomePanel.btnSignUp.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
@@ -62,9 +60,21 @@ public class WelcomeFrame extends JFrame {
 				layout.show(cardPanel, WELCOMEPANEL);
 			}
 		};
-		logInPanel.btnNewButton.addActionListener(goBack);
+		logInPanel.backButton.addActionListener(goBack);
 		signUpPanel.backButton.addActionListener(goBack);
+	}
 
+	private void editPanel() {
+		cardPanel = new JPanel();
+		cardPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(cardPanel);
+		cardPanel.setLayout(new CardLayout(0, 0));
+	}
+
+	private void editFrame() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(new Rectangle(1000, 600));
 		this.setVisible(true);
 	}
 
